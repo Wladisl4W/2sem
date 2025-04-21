067e22f8d6049009f3d7c6b92ac43cee2ea90c67
Wladisl4W <feed45537@gmail.com>
2025-04-21 11:41:19 +0300

    пароль поменял хеш

--- 6/db.sql
+++ 6/db.sql
@@ -59,7 +59,8 @@ CREATE TABLE users (
    PRIMARY KEY (admin_id)
  ) ENGINE = InnoDB;
  
- -- Добавление администратора
+ -- Добавление администратора 
 DELETE FROM admin_users WHERE admin_login = 'admin';
 INSERT INTO admin_users (admin_login, password_hash) VALUES
 ('admin', '$2y$10$eImiTXuWVxfM37uY4JANjQe5Jxq2p5s8F5l5a5l5a5l5a5l5a5l5a'); -- Хэш для пароля "123"
+UPDATE admin_users SET password_hash = '$2y$10$PZWYz2Fcd0wPAYzUH5Lfw.aDbXuzV2GxgBjqQaYQIusGhh/tEwTC2' WHERE admin_login = 'admin';
