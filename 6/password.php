2fb023b38e3b5030365fe2c38fd4661e12c8d5e8
Wladisl4W <feed45537@gmail.com>
2025-04-21 11:38:36 +0300

    123

--- 6/password.php
+++ 6/password.php
@@ -1,10 +1,3 @@
 <?php
-$password = '123';
-$hash = '$2y$10$eImiTXuWVxfM37uY4JANjQe5Jxq2p5s8F5l5a5l5a5l5a5l5a5l5a';
-
-if (password_verify($password, $hash)) {
-    echo "Пароль верный!";
-} else {
-    echo "Пароль неверный!";
-}
+echo password_hash('123', PASSWORD_DEFAULT);
 ?>
