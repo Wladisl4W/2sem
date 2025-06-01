2c5e7e774d476e2d78bc27e5f3887cbcfe514ef0
Wladisl4W <feed45537@gmail.com>
2025-06-01 20:56:18 +0300

    откакт

--- final/back/form.php
+++ final/back/form.php
@@ -22,51 +22,6 @@ $languages = isset($_SESSION['values']['lang']) ? $_SESSION['values']['lang'] : 
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<video id="headerBackgroundVideo" loop="loop" autoplay="autoplay" preload="auto" muted>
-        <source src="../images/header_background.mp4"></source>
-        <source src="../images/header_background.webm" type="video/webm"></source>
-    </video>
-    <img id="headerBackgroundImage" src="../images/header_kostyl.png">
-    
-    <header>
-        <div id="title">
-            <span>
-                <a href="../index.php">
-                    Поддержка сайтов<br>на костылях
-                </a>
-            </span>
-        </div>
-        
-        <nav>
-            <div class="nav-item dropdown">
-                контакты
-                <div class="dropdown-wrapper">
-                    <div class="dropdown-item">
-                        <a href="https://www.youtube.com/watch?v=nGBYEUNKPmo">внеконтакта</a>
-                    </div>
-                    <div class="dropdown-item">
-                        <a href="https://www.youtube.com/watch?v=nGBYEUNKPmo">телетонна</a>
-                    </div>
-                    <div class="dropdown-item">
-                        <a href="https://www.youtube.com/watch?v=nGBYEUNKPmo">гитангар</a>
-                    </div>
-                    <div class="dropdown-item">
-                        <a href="https://www.youtube.com/watch?v=nGBYEUNKPmo">крутой текст</a>
-                    </div>
-                </div>
-            </div>
-            <div class="nav-item">
-                <a href="./about_us.html">
-                    <span>о нас</span>
-                </a>
-            </div>
-            <div class="nav-item" style="cursor: wait;">
-                <span>крутой текст</span>
-            </div>
-        </nav>
-    </header>
-
     <div class="container-wide">
         <div class="header-box">Регистрация</div>
 
